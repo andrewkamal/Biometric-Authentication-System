c244d444ae9c2c4d408cf33fb0dd4800b13a5922
andrewkamal <45172552+andrewkamal@users.noreply.github.com>
2021-12-04 02:08:03 +0200

    Add files via upload

---
+++ NFC/MDK-ARM/NFC2/pn532.d
@@ -0,0 +1,5 @@
+nfc2\pn532.o: ..\Core\Src\pn532.c
+nfc2\pn532.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdio.h
+nfc2\pn532.o: ../Core/Inc/pn532.h
+nfc2\pn532.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+nfc2\pn532.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdbool.h
